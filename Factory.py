bc1cf5be47c1b176cb625963bae09e1eed2b002d
Jeremy <lznjeremy@gmail.com>
2020-05-05 11:01:59 +0300

    upload factory

---
+++ Factory.py
@@ -0,0 +1,41 @@
+from abc import ABC
+
+class Person(ABC):
+
+    def __init__(self, name, password):
+        self.name=name
+        self.password=password
+        print("Person constructor")
+
+class Client(Person):
+
+    def __init__(self, name, password):
+        Person.__init__(self, name, password)
+        print("Client constructor")
+
+class Admin(Person):
+
+    def __init__(self, name, password):
+        Person.__init__(self, name, password)
+        print("Admin constructor")
+
+class Manager(Person):
+
+    def __init__(self, name, password):
+        Person.__init__(self, name, password)
+        print("Manager constructor")
+
+class Factory:
+
+       @classmethod
+       def createPerson(self, name, password):
+           if name=="Bob" and password=="123":
+               return Admin(name, password)
+
+           if name=="Alice" and password=="678":
+               return Manager(name, password)
+               
+            return Client(name, password)
+
+
+p=Factory.createPerson("Tom", "123")
